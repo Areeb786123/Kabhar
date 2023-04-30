422eb41485d879ac326e2290ec7489e5c4fd78b4
areeb786 <areebrockzz786@gmail.com>
2023-04-30 16:06:01 +0530

    added bottom navigation

--- app/src/main/java/com/areeb/kabhar/MainActivity.kt
+++ app/src/main/java/com/areeb/kabhar/MainActivity.kt
@@ -1,17 +1,50 @@
 package com.areeb.kabhar
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.areeb.kabhar.ui.home.viewModels.HomeViewModels
+import androidx.fragment.app.Fragment
+import com.areeb.kabhar.databinding.ActivityMainBinding
+import com.areeb.kabhar.ui.home.fragment.HomeFragment
+import com.areeb.kabhar.ui.saved.SavedFragment
+import com.areeb.kabhar.ui.search.fragment.SearchFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val viewModel: HomeViewModels by viewModels()
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        viewModel.getTopHeadlines(country = "in")
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding!!.root)
+        init()
+    }
+
+    private fun init() {
+        replaceFragment(HomeFragment())
+        settingBottomNavigation()
+    }
+
+    private fun settingBottomNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.searchFragment -> replaceFragment(SearchFragment())
+                R.id.savedFragment -> replaceFragment(SavedFragment())
+            }
+            true
+        }
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.frameLayout.id, fragment).commit()
     }
 }

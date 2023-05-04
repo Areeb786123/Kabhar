98ebed69091087c69e5c4fc52d7c0791cd3c8aa9
areeb786 <areebrockzz786@gmail.com>
2023-05-05 00:42:28 +0530

    added detail screen but not getting the data

---
+++ app/src/main/java/com/areeb/kabhar/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.areeb.kabhar.utils
+
+interface Constants {
+
+    interface String_CONSTANTS {
+        companion object {
+            val WEB_VIEW_URL = "web_view_url"
+        }
+    }
+}
